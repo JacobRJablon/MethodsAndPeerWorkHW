e6b72de3f738d52907ee40427df18fe8052961b9
Jacob Jablon (RIT Student) <jrj8250@rit.edu>
2024-10-09 14:48:26 -0400

    Added in my variables, console statements, and add function

--- MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
+++ MethodsAndPeerWork/MethodsAndPeerWork/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // declaring our favorite numbers
+            int jacobFavoriteNumber;
+            // Jayson declare favNum
+
+            // initialize
+            jacobFavoriteNumber = 10;
+            // Jayson initialize favNum
+
+            // start of our actual program
+            Console.WriteLine("I am Jacob and my favorite number is: " +  jacobFavoriteNumber);
+            // Jayson console statement
+
+            // calling the add function
+            int jacobAddedNums = Add(10, 2);
+            Console.WriteLine("Jacob's added numbers: " + jacobAddedNums);
+
+            // calling the subtract function
+            // Jayson calls sub function
+            // Jayson console statement
+        }
+
+        static int Add(int param1, int param2)
+        {
+            return param1 + param2;
         }
     }
 }
